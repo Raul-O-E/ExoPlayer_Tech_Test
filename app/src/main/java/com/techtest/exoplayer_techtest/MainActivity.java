97886e0e0ef0da8fe08986120649d94a3168cb47
Raul Oliva Escribano <raul.oliva.escribano@gmail.com>
2021-06-07 07:17:33 +0200

    Initial commit

---
+++ app/src/main/java/com/techtest/exoplayer_techtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.techtest.exoplayer_techtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
